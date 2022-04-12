c97b865ca5882b55e533efb64eac5e5e197e566c
Ashley Mattam <amattam427@gmail.com>
2022-04-12 13:21:42 -0400

    commit tree 221d16c49edb94b46d24f3fc33787a24faae8070

--- index.js
+++ index.js
@@ -1,13 +1,38 @@
+function reverse(word){
+  //'abc'=>'cba'
+  //const wordArray = word.split("");
+  //const reversedWordArray = wordArray.reverse();
+  //const reversedWord = reversedWordArray.join("")
+  //return reversedWord;
+  return word.split("").reverse().join("");
+}
+
+
 function isPalindrome(word) {
   // Write your algorithm here
+  const reversedWord = reverse(word);
+  
+  if (word === reversedWord){
+    return true
+  } else {
+    return false
+  }
 }
 
 /* 
   Add your pseudocode here
+  -If word is the same as the word in reverse, return true, else false 
+
 */
 
 /*
   Add written explanation of your solution here
+  -create reverse function w/ parameter of word
+  -make word/string into an array using split method
+  -reverse the array using reverse method 
+  -join the reversed array tg with join method
+  -pass reverse(word) to isPalindrome(word)
+  -word parameter in palindrome MUST match the reverse()
 */
 
 // You can run `node index.js` to view these console logs
@@ -20,6 +45,15 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", isPalindrome("robot"));
+
+  console.log("");
+
+  console.log("Expexting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("ba"));
 }
 
 module.exports = isPalindrome;
